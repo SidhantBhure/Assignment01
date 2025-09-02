5625de82ce9f3e2e20a5c3a57301b3f2548b6bf0
SidhantBhure <sidhantbhure19@gmail.com>
2025-09-02 22:36:47 +0530

    Update PrintDetails.java

--- PrintDetails.java
+++ PrintDetails.java
@@ -2,16 +2,14 @@ import java.time.LocalDate;
 
 public class PrintDetails {
     public static void main(String[] args) {
-        // Your details
         String name = "Sidhant Bhure"; 
-        String college = "WIT Solapur";
-
-        // Get current date
+        String college = "WIT Solapur"
+            
         LocalDate currentDate = LocalDate.now();
 
-        // Print details
         System.out.println("Name: " + name);
         System.out.println("College: " + college);
         System.out.println("Current Date: " + currentDate);
     }
 }
+
